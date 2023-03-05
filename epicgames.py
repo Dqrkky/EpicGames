88653d8b1aadb24e7d928903d67f3d161489f5f1
Dqrkky <70263848+Dqrkky@users.noreply.github.com>
2023-03-05 03:28:54 +0200

    Add files via upload

---
+++ epicgames.py
@@ -0,0 +1,56 @@
+from selenium import webdriver
+import json, urllib
+
+keywords = input("What do you want to search on EpicGames : ")
+
+params = urllib.parse.urlencode(
+    query={
+        "operationName": "searchStoreQuery",
+        "variables": json.dumps(
+            obj={
+                "allowCountries": "GR",
+                "category": "games/edition/base|bundles/games|games/edition|editors|addons|games/demo|software/edition/base",
+                "count": 40,
+                "country": "GR",
+                "keywords": keywords,
+                "locale": "en-US",
+                "sortBy": "relevancy,viewableDate",
+                "sortDir": "DESC,DESC",
+                "tag": "",
+                "withPrice": True
+            }
+        ),
+        "extensions": json.dumps(
+            obj={
+                "persistedQuery": {
+                    "version": 1,
+                    "sha256Hash": "7d58e12d9dd8cb14c84a3ff18d360bf9f0caa96bf218f2c5fda68ba88d68a437"
+                }
+            }
+        )
+    }
+)
+url = f"https://store.epicgames.com/graphql?{params}"
+
+driver = webdriver.Chrome(executable_path="E:\Downloads\chromedriver_win32\chromedriver.exe")
+driver.get(url)
+
+a = None
+for ii in driver.page_source.replace("\n", "").split(">"):
+    if ii.startswith("{"):
+        a = ii
+if a != None:
+    for ii in a.split("<"):
+        if ii.endswith("}"):
+            a = ii
+
+if a != None:
+    epic = json.loads(
+        s=a
+    )
+    elements = epic["data"]["Catalog"]["searchStore"]["elements"]
+    #for element in elements:
+    #    print(element)
+    #    print("\n\n")
+    print(elements[0])
+    #https://store.epicgames.com/en-US/p/{productSlug}
